f463f47048d35bf315a65b3131001d16d4b12bb8
MichaelBezzina <michaelbezzina@outlook.com>
2022-10-24 17:48:56 -0600

    Adding Unity Project

---
+++ LightingIssue_Demo/Assets/Scripts/ExampleAssetBundleLoaderAndInstantiator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class ExampleAssetBundleLoaderAndInstantiator : MonoBehaviour
+{
+    [HideInInspector] public List<AssetBundle> downloadedAssetBundleList = new List<AssetBundle>();
+
+    private string pathToAssetBundle;
+
+    private void OnEnable()
+    {
+        SetupAssetBundlesFromServer();
+    }
+
+    private void SetupAssetBundlesFromServer()
+    {
+        pathToAssetBundle = Application.streamingAssetsPath + "/AssetBundles/Editor/examplebundle";
+
+        StartCoroutine(GetAssetBundle(pathToAssetBundle));
+    }
+
+    IEnumerator GetAssetBundle(string path)
+    {
+        using (UnityWebRequest bundle = UnityWebRequestAssetBundle.GetAssetBundle(path))
+        {
+            var request = bundle.SendWebRequest();
+            while (!request.isDone)
+                yield return null;
+
+            AssetBundle loadedAssetBundle = DownloadHandlerAssetBundle.GetContent(bundle);
+
+            if (!downloadedAssetBundleList.Contains(loadedAssetBundle)) // If Asset Bundle has not already been added to our Asset Bundle list... add it
+            {
+                Debug.Log("Asset Bundle Added");
+                downloadedAssetBundleList.Add(loadedAssetBundle);
+
+                GetPrefabFromAssetBundle("apartment");
+            }
+        }
+    }
+
+    public GameObject GetPrefabFromAssetBundle(string prefabName) // Called from SceneLoader & SpawnSystem_Items
+    {
+        GameObject loadedPrefab = null;
+        prefabName = prefabName.ToLower();
+        bool foundPrefab = false;
+        string itemName;
+
+        for (int i = 0; i < downloadedAssetBundleList.Count; i++)
+        {
+            foreach (string itemPath in downloadedAssetBundleList[i].GetAllAssetNames())
+            {
+                itemName = Path.GetFileNameWithoutExtension(itemPath).ToLower();
+
+                if (prefabName.Equals(itemName))
+                {
+                    loadedPrefab = downloadedAssetBundleList[i].LoadAsset(prefabName) as GameObject;
+                    foundPrefab = true;
+
+                    InstantiateGameObject(loadedPrefab);
+                    break;
+                }
+            }
+
+            if (foundPrefab)
+                break;
+        }
+
+        return loadedPrefab;
+    }
+
+    private void InstantiateGameObject(GameObject objectToInstantiate)
+    {
+        Instantiate(objectToInstantiate, this.transform);
+    }
+}
---
+++ LightingIssue_Demo/Assets/Scripts/InitializationManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class InitializationManager : MonoBehaviour
+{
+    public static InitializationManager Instance { get; private set; }
+
+    // VARIABLES
+    [SerializeField] private bool loadCoreAssetBundleAsAScene;
+
+    [HideInInspector] public AssetBundle coreAssetBundle;
+    private GameObject coreAssetBundlePrefab;
+    [HideInInspector] public GameObject coreAssetBundlePrefabInstance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        SetupCoreAssetBundle();
+    }
+
+    private void SetupCoreAssetBundle()
+    {
+        string coreABPath = Path.Combine(Application.streamingAssetsPath, "AssetBundles/Editor/core_example");
+
+        StartCoroutine(GetCoreAssetBundle(coreABPath));
+    }
+
+    IEnumerator GetCoreAssetBundle(string path)
+    {
+        using (UnityWebRequest bundle = UnityWebRequestAssetBundle.GetAssetBundle(path))
+        {
+            yield return bundle.SendWebRequest();
+
+            if (bundle.isNetworkError || bundle.isHttpError)
+                Debug.Log("Error in GetBundle: " + bundle.error);
+            else
+            {
+                coreAssetBundle = DownloadHandlerAssetBundle.GetContent(bundle);
+                UnpackAssetBundle(coreAssetBundle);
+            }
+        }
+
+        yield return null;
+    }
+
+    public void UnpackAssetBundle(AssetBundle ab)
+    {
+        if (loadCoreAssetBundleAsAScene)
+        {
+            string[] scenePath = ab.GetAllScenePaths();
+
+            for (int i = 0; i < scenePath.Length; i++)
+                Debug.Log("ScenePath = " + scenePath[i].ToString());
+
+            SceneManager.LoadScene(scenePath[0]);
+        }
+        else if (coreAssetBundlePrefab == null)
+        {
+            foreach (string goName in ab.GetAllAssetNames())
+            {
+                //Debug.Log(goName);
+                if (goName != null)
+                {
+                    /*string[] scenePath = ab.GetAllScenePaths();
+
+                    for(int i = 0; i < scenePath.Length; i++)
+                        Debug.Log(scenePath[i].ToString());
+
+                    SceneManager.LoadScene(scenePath[0]);
+                    */
+                    coreAssetBundlePrefab = ab.LoadAsset(goName) as GameObject;
+                    break;
+                }
+            }
+
+            if (coreAssetBundlePrefab != null)
+                coreAssetBundlePrefabInstance = Instantiate(coreAssetBundlePrefab, this.transform);
+            else
+                Debug.LogError("Failed to instantiate the core asset bundle.");
+        }
+    }
+}
