2bb039e10a6c11137cee791652f88cd7259cbf68
MichaelBezzina <michaelbezzina@outlook.com>
2022-10-25 15:57:46 -0600

    Added prefabs that need to be baked into the project
    Located at: Assets\Prefabs\AssetBundlePrefabs\SpawnableObjects\EnterAndExplore

---
+++ LightingIssue_Demo/Assets/Scripts/Utility/AssetBundleBuilder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetBundleBuilder : Editor
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        /*BuildPipeline.BuildAssetBundles(Application.dataPath + "/AssetBundles", 
+        BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.WebGL);*/
+
+        string folderName = "AssetBundles/WebGL";
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
+
+        //Build for WebGL platform
+        BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.WebGL);
+
+        //Refresh the Project folder
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Assets/Build AssetBundles For Editor")]
+    static void BuildAllAssetBundlesForEditor()
+    {
+        string folderName = "AssetBundles/Editor";
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
+
+        //Build for Windows platform - Compatible with in-editor testing, unlike WebGL
+        BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+
+        //Refresh the Project folder
+        AssetDatabase.Refresh();
+    }
+}
